e60866e86964418b91f59fe8711273441e3c59b8
Momin799 <87771916+Momin799@users.noreply.github.com>
2021-08-24 10:15:27 +0500

    Add files via upload

---
+++ src/Furnace/Main.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Furnace:
+
+use Furnace\Commands\CraftCommand;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\block\Block;
+use pocketmine\block\BlockFactory;
+use pocketmine\entity\Entity;
+use pocketmine\item\enchantment\Enchantment;
+use pocketmine\item\enchantment\EnchantmentInstance;
+use pocketmine\item\Item;
+use pocketmine\item\ItemFactory;
+
+use pocketmine\tile\Tile;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase { 
+	
+	/** @var array */
+	public $plugin;
+
+  public function onLoad(){
+    $this->getLogger()->info("Loading Plugin");
+  }
+
+  public function onDisable(){
+    $this->getLogger()->info("Plugin Disabled");
+  }
+  
+  public function onEnable(): void{
+  	$this->getServer()->getCommandMap()->register("furnace", new FurnaceCommand("furnace", $this));
+  	}
+
+}
